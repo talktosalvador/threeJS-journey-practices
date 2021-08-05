a3ca90311f8d1791e300af250b87ad2ae35a8add
Salvador Vilca <slvdrvlc@gmail.com>
2021-08-05 18:29:57 -0400

    lesson 7 cameras finished

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 /**
  * Base
@@ -19,17 +20,32 @@ const scene = new THREE.Scene()
 // Object
 const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    new THREE.MeshBasicMaterial({color: 0xff0000})
 )
 scene.add(mesh)
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
+/**
+ * Camera
+ * */
+/* Orthographic camera */
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, .1, 100)
+/* Perspective camera */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, .1, 100)
+// camera.lookAt(mesh.position)
+// camera.position.x = 2
+// camera.position.y = 2
 camera.position.z = 2
-camera.lookAt(mesh.position)
 scene.add(camera)
+
+/**
+ * OrbitControls
+ * */
+const controls = new OrbitControls(camera, canvas)
+// controls.target.y = 2
+controls.enableDamping = true
+controls.autoRotate = true
+controls.autoRotateSpeed = 2
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -37,15 +53,36 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-// Animate
-const clock = new THREE.Clock()
+/**
+ * Cursor
+ * */
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - .5
+    cursor.y = event.clientY / sizes.height - .5
+    // console.log(cursor.x, cursor.y)
+})
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+/**
+ * Animate
+ */
+// const clock = new THREE.Clock()
+const tick = () => {
 
-    // Update objects
-    mesh.rotation.y = elapsedTime;
+    /* Update objects */
+    // const elapsedTime = clock.getElapsedTime()
+    // mesh.rotation.y = elapsedTime;
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
+    // camera.position.y = Math.sin(cursor.y * Math.PI * 2) * 2
+    // camera.position.y = cursor.y * 3
+    // camera.lookAt(mesh.position)
+
+    /* controls */
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
@@ -55,3 +92,4 @@ const tick = () =>
 }
 
 tick()
+
