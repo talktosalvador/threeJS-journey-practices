472f21ffa42e8e326247be507485ad1f89ff1793
Salvador Vilca <slvdrvlc@gmail.com>
2021-08-06 02:50:28 -0400

    lesson 9 geometries finished

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -11,13 +11,36 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+/**
+ * Object
+ * */
+// const geometry = new THREE.SphereGeometry(1,32,32)
+/* Create an empty BufferGeometry */
+const geometry = new THREE.BufferGeometry()
+/* Create a Float32Array containing the vertices position (3 by 3) */
+// const positionsArray = new Float32Array([
+//     0, 0, 0, // First vertex
+//     0, 1, 0, // Second vertex
+//     1, 0, 0  // Third vertex
+// ])
+// Create 50 triangles (450 values)
+const count = 50
+const positionsArray = new Float32Array(count * 3 * 3)
+for(let i = 0; i < count * 3 * 3; i++)
+{
+    positionsArray[i] = (Math.random() - 0.5) * 4
+}
+/* Create the attribute and name it 'position' */
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 , wireframe : true})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-// Sizes
+
+/**
+ * Sizes
+ * */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
