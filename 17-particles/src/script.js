2d6ce70808c849219b1a25abf8e386f022f0a288
Salvador Vilca <slvdrvlc@gmail.com>
2021-08-14 20:08:31 -0400

    lesson 17 particles finished

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -1,6 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
 /**
@@ -19,15 +19,58 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/10.png')
 
 /**
- * Test cube
+ * Material
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxBufferGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.5,
+    sizeAttenuation: true
+})
+particlesMaterial.color = new THREE.Color('red')
+// particlesMaterial.map = particleTexture
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+// particlesMaterial.alphaTest = 0.001
+// particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true
+
+/**
+ * Particles
+ */
+// Geometry
+// const particlesGeometry = new THREE.SphereGeometry(1, 64, 64)
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 500
+
+const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
+const colors = new Float32Array(count * 3)
+
+for (let i = 0; i < count * 3; i++) // Multiply by 3 for same reason
+{
+    positions[i] = (Math.random() - 0.5) * 3 // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+    colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+// Cube
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(),
+//     new THREE.MeshBasicMaterial({
+//         // color:new THREE.Color('purple')
+//     })
+// )
+// scene.add(cube)
 
 /**
  * Sizes
@@ -37,8 +80,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -57,7 +99,8 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.z = 2
+// camera.position.y = 1
 scene.add(camera)
 
 // Controls
@@ -78,9 +121,18 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+    // Update particles
+    // particles.rotation.z = Math.cos(elapsedTime * 0.1) * Math.PI * 2
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime)
+        // particlesGeometry.attributes.position.array[i3 + 2] = 0//Math.sin(elapsedTime)
+        const x = particlesGeometry.attributes.position.array[i3 + 2]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime * 0.2 + x * 6)
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
