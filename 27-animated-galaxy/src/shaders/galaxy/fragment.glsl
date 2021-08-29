ecff48a814ce11387015174b0208ff4ec768c766
Salvador Vilca <slvdrvlc@gmail.com>
2021-08-28 21:47:54 -0400

    lesson 27 animated galaxy finished

---
+++ 27-animated-galaxy/src/shaders/galaxy/fragment.glsl
@@ -0,0 +1,15 @@
+uniform float uAlpha;
+
+varying vec3 vColor;
+
+void main()
+{
+    // Light point
+    float strength = distance(gl_PointCoord, vec2(0.5));
+    strength = 1.0 - strength;
+    strength = pow(strength, 5.0);
+
+    // Final color
+    vec3 color = mix(vec3(0.0), vColor, strength);
+    gl_FragColor = vec4(color, uAlpha);
+}
