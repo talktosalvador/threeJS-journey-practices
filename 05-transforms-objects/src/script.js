63b6c6767c180b0d0f2929da86e30552fbf87491
Salvador Vilca <salvasvip@gmail.com>
2022-09-07 17:23:47 -0400

    finding & fixing problems with eslint

--- 05-transforms-objects/src/script.js
+++ 05-transforms-objects/src/script.js
@@ -1,71 +1,70 @@
-import './style.css'
-import * as THREE from 'three'
+import "./style.css";
+import * as THREE from "three";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Axes Helper
  */
-const axesHelper = new THREE.AxesHelper(5)
-scene.add(axesHelper)
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
 
 /**
  * Objects
  */
-const group = new THREE.Group()
-group.scale.y = 2
-group.rotation.y = 0.2
-scene.add(group)
+const group = new THREE.Group();
+group.scale.y = 2;
+group.rotation.y = 0.2;
+scene.add(group);
 
 const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xffaa00 })
-)
-cube1.position.x = - 1.5
-group.add(cube1)
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xffaa00 })
+);
+cube1.position.x = -1.5;
+group.add(cube1);
 
 const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xffaa00 })
-)
-cube2.position.x = 0
-group.add(cube2)
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xffaa00 })
+);
+cube2.position.x = 0;
+group.add(cube2);
 
 const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xffaa00 })
-)
-cube3.position.x = 1.5
-group.add(cube3)
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xffaa00 })
+);
+cube3.position.x = 1.5;
+group.add(cube3);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.set(1, 1, 5)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.set(1, 1, 5);
+scene.add(camera);
 
-camera.lookAt(group.position)
-
+camera.lookAt(group.position);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
+  canvas,
+});
 
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
