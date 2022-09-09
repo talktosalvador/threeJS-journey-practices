014274101a8f9ba248b454a7022242b446a08993
Salvador Vilca <salvasvip@gmail.com>
2022-09-08 22:26:28 -0400

    finding & fixing problems with eslint

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -1,71 +1,71 @@
-import './style.css'
-import * as THREE from 'three'
-import gsap from 'gsap'
+import "./style.css";
+import * as THREE from "three";
+import gsap from "gsap";
 
-/*Canvas*/
-const canvas = document.querySelector('canvas.webgl')
+/* Canvas */
+const canvas = document.querySelector("canvas.webgl");
 
-/*Scene*/
-const scene = new THREE.Scene()
+/* Scene */
+const scene = new THREE.Scene();
 
-/*Object*/
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: 0xff0000})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+/* Object */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-/*Sizes*/
+/* Sizes */
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
-/*Camera*/
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+/* Camera */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
-/*Renderer*/
+/* Renderer */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
+  canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
 
-/*Animate*/
+/* Animate */
 // let time = Date.now()
 // const clock = new THREE.Clock()
 
-gsap.to(mesh.position, {duration: 1, delay: 0, x: 2})
+gsap.to(mesh.position, { duration: 1, delay: 0, x: 2 });
 const tick = () => {
-    /*Time*/
-    // const currentTime = Date.now()
-    // const deltaTime = currentTime - time
-    // time = currentTime
-    // console.log(deltaTime)
-    // const elapsedTime = clock.getElapsedTime()
-    // console.log(elapsedTime)
+  /* Time */
+  // const currentTime = Date.now()
+  // const deltaTime = currentTime - time
+  // time = currentTime
+  // console.log(deltaTime)
+  // const elapsedTime = clock.getElapsedTime()
+  // console.log(elapsedTime)
 
-    /*Update an object(s)*/
-    // mesh.scale.y -= .01
-    // mesh.rotation.x += .01
+  /* Update an object(s) */
+  // mesh.scale.y -= .01
+  // mesh.rotation.x += .01
 
-    // mesh.rotation.y += .01 * deltaTime
+  // mesh.rotation.y += .01 * deltaTime
 
-    // mesh.rotation.z = elapsedTime
+  // mesh.rotation.z = elapsedTime
 
-    // mesh.position.x = Math.cos(elapsedTime)
-    // mesh.position.y = Math.cos(elapsedTime)
-    // mesh.position.z = Math.cos(elapsedTime)
+  // mesh.position.x = Math.cos(elapsedTime)
+  // mesh.position.y = Math.cos(elapsedTime)
+  // mesh.position.z = Math.cos(elapsedTime)
 
-    // camera.position.x = Math.cos(elapsedTime)
-    // camera.position.y = Math.sin(elapsedTime)
-    // camera.lookAt(mesh.position)
+  // camera.position.x = Math.cos(elapsedTime)
+  // camera.position.y = Math.sin(elapsedTime)
+  // camera.lookAt(mesh.position)
 
-    /*Renderer*/
-    renderer.render(scene, camera)
+  /* Renderer */
+  renderer.render(scene, camera);
 
-    /*Call this function again on the next frame*/
-    window.requestAnimationFrame(tick)
-}
+  /* Call this function again on the next frame */
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
